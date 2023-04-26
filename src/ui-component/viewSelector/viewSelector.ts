38e21bc8b4d20e631e5e9773563f3273e036b4db
jeehunter <709121402@qq.com>
2023-04-26 23:20:52 +0800

    feat:sidebar viewSelector

--- src/ui-component/viewSelector/viewSelector.ts
+++ src/ui-component/viewSelector/viewSelector.ts
@@ -5,15 +5,43 @@ import { IViewPath } from '../../server/viewService';
 
 export class ViewSelector extends BaseComponent implements IDisposable {
     element: HTMLElement;
+    viewItems: HTMLElement[] = [];
+    viewPaths: IViewPath[] = [];
 
     constructor(viewPaths: IViewPath[], size: ISize) {
         super();
         this.element = document.createElement('ul');
+        this.element.classList.add('view-selector');
         this.shadow.appendChild(this.element);
+        for (const viewPath of viewPaths) {
+            const viewItem = this.createViewItem(viewPath);
+            this.viewItems.push(viewItem);
+            this.viewPaths.push(viewPath);
+            this.element.appendChild(viewItem);
+        }
+        if(this.viewPaths.length){
+            this.selectView(this.viewPaths[0].id);
+        }
+    }
+
+    private createViewItem(viewPath: IViewPath) {
+        const item = document.createElement('li');
+        item.classList.add('view-item-container');
+        item.id = viewPath.id;
+        const itemLabel = document.createElement('a');
+        itemLabel.href = viewPath.path;
+        itemLabel.innerText = viewPath.displayName;
+        itemLabel.classList.add('view-item-label');
+        item.appendChild(itemLabel);
+        return item;
     }
 
     setSize(size: ISize) {
         this.element.style.width = size.width + 'px';
+    }
+
+    selectView(id: string) {
+
     }
 
     dispose(): void {
